69d170439dd6745dd0b7b9ac73cda45f07f5d13f
Leonardo Barozzi <leonardo.barozzi@gmail.com>
2025-11-20 12:51:41 +0100

    ggplot demo

---
+++ demo.r
@@ -0,0 +1,25 @@
+# install.packages("tidyverse")
+library(tidyverse)
+
+dtm<-mpg
+
+
+ggplot(data=dtm)
+
+ggplot(data=dtm) + geom_point(mapping = aes(x=displ, y = hwy))
+ggplot(data=dtm) + geom_point(mapping = aes(x=displ, y = hwy, color=class))
+ggplot(data=dtm) + geom_point(mapping = aes(x=year, y = class, color=hwy, size=displ))
+
+ggplot(data=dtm) + geom_point(mapping= aes(x=displ, y=hwy, shape=class, size=cty))
+
+ggplot(data=dtm) + geom_point(mapping= aes(x=displ, y=hwy, color=class)) + facet_wrap(~ class, nrow=2)
+
+ggplot(data=dtm) + geom_point(mapping= aes(x=displ, y=hwy, color=class)) + facet_grid( drv ~ cyl)
+
+ggplot(data=dtm) + geom_point(mapping= aes(x=displ, y=hwy, color=manufacturer)) + facet_wrap(~ class, nrow=2)
+
+ggplot(data=dtm) + geom_point(mapping= aes(x=displ, y=hwy, color=manufacturer)) + geom_smooth(mapping=aes(x=displ,y=hwy))
+
+#Lab 1
+#  show data significantly
+
